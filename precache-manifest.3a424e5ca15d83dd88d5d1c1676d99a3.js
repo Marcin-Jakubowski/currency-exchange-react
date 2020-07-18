bd58d65de7409e2203c87c58e7b20600150978a6
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-18 19:53:43 +0200

    Updates

---
+++ precache-manifest.3a424e5ca15d83dd88d5d1c1676d99a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93c6f6132e6f573602db73ea7310f692",
+    "url": "/currency-exchange-react/index.html"
+  },
+  {
+    "revision": "81280a9922c704df5846",
+    "url": "/currency-exchange-react/static/css/main.f0460a2a.chunk.css"
+  },
+  {
+    "revision": "18d69a9577d9a4fe975d",
+    "url": "/currency-exchange-react/static/js/2.4befac21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-exchange-react/static/js/2.4befac21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81280a9922c704df5846",
+    "url": "/currency-exchange-react/static/js/main.769a3485.chunk.js"
+  },
+  {
+    "revision": "7d1d74d1656f6e6300ad",
+    "url": "/currency-exchange-react/static/js/runtime-main.edaef514.js"
+  }
+]);
