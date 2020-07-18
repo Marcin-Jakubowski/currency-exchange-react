f74e6b696fe65253aeee176d4d2b7fd45e6d5e4b
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-18 17:07:48 +0200

    prepared files to currency exchange react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main className="container">
+    <h1 className="header">Kantor wymiany walut</h1>
+    <div className="section">
+        <form className="form">
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Wypełnij dane:</legend>
+
+
+                    <label className="form__label">
+                        <span className="form__text">Podaj kwotę: </span><input name="amount" value="" required type="number" step="0.01" min="5" max="10000000" autofocus />
+                    </label>
+
+                    <label className="form__label">
+                        <span className="form__text">Podaj walutę początkową: </span><select name="actualCurrency">
+                            <option value="PLN">PLN</option>
+                            <option value="EUR">Euro</option>
+                            <option value="USD">Dolar amerykański</option>
+                        </select>
+                    </label>
+
+                    <label className="form__label">
+                        <span className="form__text">Podaj walutę końcową: </span><select name="expectedCurrency">
+                            <option value="PLN">PLN</option>
+                            <option value="EUR">Euro</option>
+                            <option value="USD">Dolar amerykański</option>
+                        </select>
+                    </label>
+
+                <button className="form__button">Przelicz</button>
+
+            </fieldset>
+        </form>
+        <p className="section__paragraph">Otrzymasz: <strong>N/a</strong></p>
+        
     </div>
+</main>
   );
 }
 
