947adc184a5f5084312ca3ef1a7bd64f3abb76dc
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-18 19:49:41 +0200

    added Section component

--- src/Section/index.js
+++ src/Section/index.js
@@ -1,2 +1,13 @@
 import React from "react";
 import "./style.css"
+
+const Section = ({body, finalResult}) => (
+    <div className="section">
+        {body};
+        <p className="section__paragraph">Otrzymasz: <strong>{finalResult.toFixed(2)}</strong></p>
+        
+    </div>
+);
+
+
+export default Section;
