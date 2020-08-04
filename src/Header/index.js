4ba01c5deb875631ec171fa97acd666bc0577cb9
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-08-04 15:06:22 +0200

    changing moment of date formatting

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,31 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
 
+const dateFormat = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+};
+
 const Header = () => {
-    const [currentDate, setCurrentDate] = useState();
+    const [currentDate, setCurrentDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            const myDate = new Date().toLocaleDateString("pl", {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                year: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-                second: "numeric",
-            });
+            const myDate = new Date();
             setCurrentDate(myDate);
         }, 1000);
 
         return () => {
             clearInterval(intervalId);
         };
-    }, []);
+    });
 
     return (
         <div className="header">
-            <p className="header__date">Dzisiaj jest {currentDate}</p>
+            <p className="header__date">Dzisiaj jest {currentDate.toLocaleDateString(undefined, dateFormat)}</p>
             <h1 className="header__paragraph">Kantor wymiany walut</h1>
         </div>
     )
