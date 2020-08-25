732c63aabe3f46db9eec518730df73d9e270ab6a
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-08-25 23:22:48 +0200

    styling Container component as styled components

---
+++ src/Container/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+    font-family: 'Lato', sans-serif;
+    margin-top: 100px;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
