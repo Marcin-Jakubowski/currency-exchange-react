02beba6c37a10f98e42ca28d6e0a6c55cc05ee5a
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-08-25 23:20:47 +0200

    styling Header component as styled components

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useCurrentDate from "./useCurrentDate";
-import "./style.css";
+import { HeaderBox, DateBox, Content } from "./styled";
 
 const dateFormat = {
     weekday: "long",
@@ -13,13 +13,13 @@ const dateFormat = {
 };
 
 const Header = () => {
-const date = useCurrentDate();
+    const date = useCurrentDate();
 
     return (
-        <div className="header">
-            <p className="header__date">Dzisiaj jest {date.toLocaleDateString(undefined, dateFormat)}</p>
-            <h1 className="header__paragraph">Kantor wymiany walut</h1>
-        </div>
+        <HeaderBox>
+            <DateBox>Dzisiaj jest {date.toLocaleDateString(undefined, dateFormat)}</DateBox>
+            <Content>Kantor wymiany walut</Content>
+        </HeaderBox>
     )
 };
 
---
+++ src/Header/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HeaderBox = styled.div`
+    text-align: center;
+    background-color: #0353A4;
+    padding: 10px 30px;
+    border-radius: 10px;
+    border: 2px solid #979DAC;
+    max-width: 500px;
+`;
+
+export const DateBox = styled.p`
+    font-family: 'Inconsolata', monospace;
+    text-align: end;
+    margin: 0;
+    font-size: 13px;
+`;
+
+export const Content = styled.h1`
+    margin: 5px 5px 20px;
+`;
