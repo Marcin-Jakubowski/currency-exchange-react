708b964e22daa7daa593131c82c542fa2c84506c
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-09-08 10:57:48 +0200

    adding information if data is importing

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FormItem, Fieldset, Legend, Label, Content, Button, DataInfo } from "./styled"
+import { FormItem, Fieldset, Legend, Label, Content, Button, DataInfo, ImportInfo } from "./styled"
 
 const Form = ({ calculateResult, ratesData }) => {
     const [amount, setAmount] = useState("");
@@ -15,49 +15,50 @@ const Form = ({ calculateResult, ratesData }) => {
         calculateResult(+amount, +currentCurrency, +expectedCurrency);
     }
 
-    return ratesData === undefined || (
-        <FormItem onSubmit={onFormSubmit}>
-            <Fieldset>
-                <Legend>Wypełnij dane:</Legend>
-                <Label>
-                    <Content>Podaj kwotę: </Content>
-                    <input
-                        value={amount}
-                        required
-                        type="number"
-                        step="0.01"
-                        min="5"
-                        max="10000000"
-                        autoFocus
-                        onChange={({ target }) => setAmount(target.value)}
-                    />
-                </Label>
-                <Label>
-                    <Content>Podaj walutę początkową: </Content>
-                    <select
-                        onChange={({ target }) => setCurrentCurrency(target.value)}
-                        value={currentCurrency}
-                    >
-                        <option></option>
-                        {generateOptions}
-                    </select>
-                </Label>
-                <Label>
-                    <Content>Podaj walutę końcową: </Content>
-                    <select
-                        onChange={({ target }) => setExpectedCurrency(target.value)}
-                        value={expectedCurrency}
-                    >
-                        <option></option>
-                        {generateOptions}
-                    </select>
-                </Label>
-                <Button>Przelicz</Button>
-                <DataInfo>Kursy walut pobierane są z Europejskiego Banku Centralnego.</DataInfo>
-                <DataInfo>Aktualne na dzień: {ratesData === undefined || ratesData.date}</DataInfo>
-            </Fieldset>
-        </FormItem>)
+    return ratesData === undefined
+        ? <ImportInfo>Trwa pobieranie aktualnych kursów walut.</ImportInfo>
+        : (
+            <FormItem onSubmit={onFormSubmit}>
+                <Fieldset>
+                    <Legend>Wypełnij dane:</Legend>
+                    <Label>
+                        <Content>Podaj kwotę: </Content>
+                        <input
+                            value={amount}
+                            required
+                            type="number"
+                            step="0.01"
+                            min="5"
+                            max="10000000"
+                            autoFocus
+                            onChange={({ target }) => setAmount(target.value)}
+                        />
+                    </Label>
+                    <Label>
+                        <Content>Podaj walutę początkową: </Content>
+                        <select
+                            onChange={({ target }) => setCurrentCurrency(target.value)}
+                            value={currentCurrency}
+                        >
+                            <option></option>
+                            {generateOptions}
+                        </select>
+                    </Label>
+                    <Label>
+                        <Content>Podaj walutę końcową: </Content>
+                        <select
+                            onChange={({ target }) => setExpectedCurrency(target.value)}
+                            value={expectedCurrency}
+                        >
+                            <option></option>
+                            {generateOptions}
+                        </select>
+                    </Label>
+                    <Button>Przelicz</Button>
+                    <DataInfo>Kursy walut pobierane są z Europejskiego Banku Centralnego.</DataInfo>
+                    <DataInfo>Aktualne na dzień: {ratesData === undefined || ratesData.date}</DataInfo>
+                </Fieldset>
+            </FormItem>)
 };
-
 
 export default Form;
